693ede6a37fd0aec9013cf261861549b2498f1d9
Martin Schvartzman <martin77s@gmail.com>
2019-11-07 00:26:31 +0200

    content updates

---
+++ PS/Test-AzNameAvailability.ps1
@@ -0,0 +1,94 @@
+﻿# Get-AzResourceProvider | Where-Object { $_.ResourceTypes.ResourceTypeName -match 'checkNameAvailability' } | Select-Object ProviderNamespace
+
+
+function Get-AccesTokenFromServicePrincipal {
+    param(
+        [string] $TenantID,
+        [string] $ClientID,
+        [string] $ClientSecret
+    )
+
+    $TokenEndpoint = { https://login.windows.net/ { 0 }/oauth2/token } -f $TenantID
+    $ARMResource = "https://management.core.windows.net/"
+
+    $Body = @{
+        'resource'      = $ARMResource
+        'client_id'     = $ClientID
+        'grant_type'    = 'client_credentials'
+        'client_secret' = $ClientSecret
+    }
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers     = @{'accept' = 'application/json' }
+        Body        = $Body
+        Method      = 'Post'
+        URI         = $TokenEndpoint
+    }
+    $token = Invoke-RestMethod @params
+    ('Bearer ' + ($token.access_token).ToString())
+}
+
+
+function Get-AccesTokenFromCurrentUser {
+    $azContext = Get-AzContext
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList $azProfile
+    $token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+    ('Bearer ' + $token.AccessToken)
+}
+
+
+function Test-AzNameAvailability {
+    param(
+        [Parameter(Mandatory = $true)] [string] $AuthorizationToken,
+        [Parameter(Mandatory = $true)] [string] $SubscriptionId,
+        [Parameter(Mandatory = $true)] [string] $Name,
+        [Parameter(Mandatory = $true)] [ValidateSet(
+            'ApiManagement', 'KeyVault', 'ManagementGroup', 'Sql', 'StorageAccount', 'WebApp')]
+        $ServiceType
+    )
+
+    $uriByServiceType = @{
+        ApiManagement   = 'https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.ApiManagement/checkNameAvailability?api-version=2019-01-01'
+        KeyVault        = 'https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.KeyVault/checkNameAvailability?api-version=2019-09-01'
+        ManagementGroup = 'https://management.azure.com/providers/Microsoft.Management/checkNameAvailability?api-version=2018-03-01-preview'
+        Sql             = 'https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Sql/checkNameAvailability?api-version=2018-06-01-preview'
+        StorageAccount  = 'https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Storage/checkNameAvailability?api-version=2019-06-01'
+        WebApp          = 'https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.Web/checkNameAvailability?api-version=2019-08-01'
+    }
+
+    $typeByServiceType = @{
+        ApiManagement   = 'Microsoft.ApiManagement/service'
+        KeyVault        = 'Microsoft.KeyVault/vaults'
+        ManagementGroup = '/providers/Microsoft.Management/managementGroups'
+        Sql             = 'Microsoft.Sql/servers'
+        StorageAccount  = 'Microsoft.Storage/storageAccounts'
+        WebApp          = 'Microsoft.Web/sites'
+    }
+
+    $uri = $uriByServiceType[$ServiceType] -replace ([regex]::Escape('{subscriptionId}')), $SubscriptionId
+    $body = '"name": "{0}", "type": "{1}"' -f $Name, $typeByServiceType[$ServiceType]
+
+    $response = (Invoke-WebRequest -Uri $uri -Method Post -Body "{$body}" -ContentType "application/json" -Headers @{Authorization = $AuthorizationToken }).content
+    $response | ConvertFrom-Json |
+        Select-Object @{N = 'Name'; E = { $Name } }, @{N = 'Type'; E = { $ServiceType } }, @{N = 'Available'; E = { $_ | Select-Object -ExpandProperty *available } }, Reason, Message
+
+}
+
+<#
+$tenantId = '72f988bf-86f1-4400-91ab-2d7cd011db47'
+$clientID = 'c9e2e0c9-af17-41af-9977-5e17e5b9b762'
+$clientSecret = ':B:yHOVO0qlx.w9j-4UCHt6Ug/1UpJK!'
+$subscriptionId = 'd75b13e4-2bf5-4c6d-86ad-c7a943a137f6'
+$AuthorizationToken = Get-AccesTokenFromServicePrincipal -TenantID $tenantId -ClientID $clientID -ClientSecret $clientSecret
+
+$AuthorizationToken = Get-AccesTokenFromCurrentUser
+#>
+
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name martin -ServiceType ApiManagement
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name kv -ServiceType KeyVault
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name root -ServiceType ManagementGroup
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name martin -ServiceType Sql
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name storage -ServiceType StorageAccount
+Test-AzNameAvailability -AuthorizationToken $AuthorizationToken -SubscriptionId $subscriptionId -Name www -ServiceType WebApp
+
