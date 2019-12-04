0ea8a9ae68d65bfebe1cf8380c067f40eb09db18
Martin Schvartzman <martin77s@gmail.com>
2019-12-04 16:55:28 +0200

    content updates

--- Runbooks/InheritTagsFromResourceGroupEx.ps1
+++ Runbooks/InheritTagsFromResourceGroupEx.ps1
@@ -5,6 +5,13 @@ PARAM(
 
 Write-Output ('{0:yyyy-MM-dd HH:mm:ss.f} - Starting' -f (Get-Date))
 
+function Compare-TagCollection {
+    param($Reference, $Difference)
+    @($Reference.GetEnumerator() | ForEach-Object {
+            $Difference[$_.Key] -eq $_.Value
+        } | Where-Object { -not $_ }).Count -eq 0
+}
+
 try {
 
     # Login to Azure
@@ -34,19 +41,24 @@ try {
                 Write-Output ('Verifying tags for {0}/{1}' -f $_.ResourceGroupName, $resource.Name)
                 $resourceid = $resource.resourceId
                 $resourcetags = $resource.Tags
+                $tagsSet = $null
 
                 if ($resourcetags -eq $null) {
-                    $tagsSet = Set-AzResource -ResourceId $resourceid -Tag $resourceGroupTags -Force
+                    $tagsSet = Set-AzResource -ResourceId $resourceid -Tag $resourceGroupTags -Force -WhatIf
                 } else {
                     $tagsToSet = $resourceGroupTags.Clone()
-                    foreach ($tag in $resourcetags.GetEnumerator()) {
-                        if ($tagsToSet.Keys -inotcontains $tag.Key) {
-                            $tagsToSet.Add($tag.Key, $tag.Value)
+                    if (-not (Compare-TagCollection -Reference $tagsToSet -Difference $resourcetags)) {
+                        foreach ($tag in $resourcetags.GetEnumerator()) {
+                            if ($tagsToSet.Keys -inotcontains $tag.Key) {
+                                $tagsToSet.Add($tag.Key, $tag.Value)
+                            }
                         }
+                        $tagsSet = Set-AzResource -ResourceId $resourceid -Tag $tagsToSet -Force -WhatIf
                     }
-                    $tagsSet = Set-AzResource -ResourceId $resourceid -Tag $tagsToSet -Force -WhatIf
                 }
-                Write-Verbose $tagsSet
+                if ($tagsSet) {
+                    Write-Output ('Tags updated for {0}/{1}' -f $_.ResourceGroupName, $resource.Name)
+                }
             }
         }
     }
