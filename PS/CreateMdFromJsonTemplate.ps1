e582aaa87910474b810c5288bc613d4d1e2087ac
Martin Schvartzman <martin77s@gmail.com>
2020-01-30 18:15:55 +0200

    content updates

--- PS/CreateMdFromJsonTemplate.ps1
+++ PS/CreateMdFromJsonTemplate.ps1
@@ -1,43 +1,57 @@
 param (
-    $Path = (Join-Path -Path $PSScriptRoot -ChildPath azuredeploy.json),
-	$OutputPath = (Join-Path -Path $PSScriptRoot -ChildPath azuredeploy.md),
+    [CmdletBinding()]
+    
+    [Parameter(Mandatory=$true)] 
+    [ValidateScript({Test-Path -Path $_})] $Path,
     [Switch]$PassThru
 )
 
-$json = ConvertFrom-Json -InputObject (Get-Content $Path -Raw) -ErrorAction Stop
 
-$parameters = @'
+$json = ConvertFrom-Json -InputObject (Get-Content $Path -Raw) -ErrorAction Stop
+$OutputPath = Join-Path -Path (Split-Path -Path $Path -Parent) -ChildPath 'readme.md'
+$parameters = $resources = $outputs = $null
+$resourceType = ((Split-Path -Path $Path -Leaf) -replace '(\.deploy)?(\.json)')
+
+if($json.parameters) {
+    $parameters = @'
 | Name | Type | Description | DefaultValue |
 | --- | --- | --- | --- |
 {0}
 '@  -f $(
-    @(foreach ($param in ($json.parameters | Get-Member -MemberType NoteProperty)) {
-        '| {0} | {1} | {2} | {3} |' -f `
-            $param.Name, $json.parameters.($param.Name).type,
-            $json.parameters.($param.Name).metadata.description,
-            $json.parameters.($param.Name).defaultValue
-    }) -join [environment]::NewLine
-)
+        @(foreach ($param in ($json.parameters | Get-Member -MemberType NoteProperty)) {
+            '| {0} | {1} | {2} | {3} |' -f `
+                $param.Name, $json.parameters.($param.Name).type,
+                $json.parameters.($param.Name).metadata.description,
+                $json.parameters.($param.Name).defaultValue
+        }) -join [environment]::NewLine
+    )
+}
 
-$resources = @'
+
+if($json.resources) {
+    $resources = @'
 | Type | Name | Comment |
 | --- | --- | --- |
 {0}
 '@ -f $(
-    (@(foreach($resource in $json.resources) {
-        '| {0} | {1} | {2} |' -f $resource.Type, $resource.Name, $resource.Comments
-    }) | Sort-Object) -join [environment]::NewLine
-)
+        (@(foreach($resource in $json.resources) {
+            '| {0} | {1} | {2} |' -f $resource.Type, $resource.Name, $resource.Comments
+        }) | Sort-Object) -join [environment]::NewLine
+    )
+}
 
-$outputs = @'
+
+if($json.outputs) {
+    $outputs = @'
 | Name | Type | Value|
 | --- | --- | --- |
 {0}
 '@ -f $(
-    @(foreach($output in ($json.outputs | Get-Member -MemberType NoteProperty)) {
-        '| {0} | {1} | {2} |' -f $output.Name , $json.outputs.($output.Name).type, $json.outputs.($output.Name).value
-    }) -join [environment]::NewLine
-)
+        @(foreach($output in ($json.outputs | Get-Member -MemberType NoteProperty)) {
+            '| {0} | {1} | {2} |' -f $output.Name , $json.outputs.($output.Name).type, $json.outputs.($output.Name).value
+        }) -join [environment]::NewLine
+    )
+}
 
 @'
 # {0}
@@ -53,5 +67,5 @@ $outputs = @'
 ## Outputs
 
 {3}
-'@ -f (Split-Path -Path $Path -Leaf), $parameters, $resources, $outputs | Out-File -FilePath $OutputPath
+'@ -f $resourceType, $parameters, $resources, $outputs | Out-File -FilePath $OutputPath -Force
 if($PassThru) { Get-Item -Path $OutputPath }
