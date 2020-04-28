2d741ec923913beb25354750130bb5f3a633629d
Martin Schvartzman <martin77s@gmail.com>
2020-04-28 20:06:54 +0300

    content updates

---
+++ PS/Get-ResourcesWithManagedIdentities.ps1
@@ -0,0 +1,21 @@
+﻿PARAM(
+    $TenantId,
+    $SubscriptionId
+)
+
+if($TenantId -and $SubscriptionId) {
+    # Login to azure and set the context:
+    Add-AzAccount -TenantId $TenantId 
+    Set-AzContext -TenantId $TenantId -SubscriptionId $SubscriptionId -Force
+}
+
+# List resources with managed identities:
+Get-AzVm | Where-Object { $_.Identity } | 
+    Select-Object @{N='ResouceType';E={$_.Type}}, @{N='ResourceGroup';E={$_.ResourceGroupName}}, Name -ExpandProperty Identity
+
+Get-AzVmss | Where-Object { $_.Identity } | 
+    Select-Object @{N='ResouceType';E={$_.Type}}, @{N='ResourceGroup';E={$_.ResourceGroupName}}, Name -ExpandProperty Identity
+
+Get-AzWebApp | Where-Object { $_.Identity } | 
+    Select-Object @{N='ResouceType';E={$_.Type}}, ResourceGroup, Name -ExpandProperty Identity
+
