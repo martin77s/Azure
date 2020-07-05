9d6282c96193c93a81a18a656432c2ca32e9488a
Martin Schvartzman <martin77s@gmail.com>
2020-07-05 13:27:01 +0300

    content updates

--- Runbooks/ShutdownVMsByMetricTag.ps1
+++ Runbooks/ShutdownVMsByMetricTag.ps1
@@ -1,75 +1,81 @@
-PARAM(
+PARAM (
     [string] $ConnectionName = 'AzureRunAsConnection',
-    [bool] $DeallocateVMs = $false
+    [string] $MetricTagName = 'ShutdownByMetric',
+    [boolean] $DeallocateVMs = $false
 )
 
 Write-Output ('{0:yyyy-MM-dd HH:mm:ss.f} - Starting' -f (Get-Date))
 
-
 try {
 
     # Authenticate to ARM
-    Disable-AzContextAutosave –Scope Process | Out-Null
     $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
-    Add-AzAccount -ServicePrincipal -Tenant $servicePrincipalConnection.TenantId `
+    $null = Add-AzAccount -ServicePrincipal -Tenant $servicePrincipalConnection.TenantId `
         -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint | Out-Null
-
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
 
     # Set the Resource Graph query
     $query = @'
 resources
-| where type == 'microsoft.compute/virtualmachines' and tags['ShutdownByMetric'] matches regex '.*'
-| extend cpuThreshold = tags.ShutdownByMetric | project id, subscriptionId, resourceGroup, name, cpuThreshold
-'@
+| where type == 'microsoft.compute/virtualmachines' and tags['{0}'] matches regex '.*'
+| extend cpuThreshold = tags.{0} | project id, subscriptionId, resourceGroup, name, cpuThreshold
+'@ -f $MetricTagName
 
     # Query Azure Resource Graph for all tagged VMs and their thresholds
     $VMs = Search-AzGraph -Query $query
 
+    # Iterate the VMs collection and determine if they should be shutdown or not
     $report = foreach ($vm in $VMs) {
 
         # Set initial flag
         $isValid = $true
+        $isBelowThreshold = $false
+        $cpuThreshold = 'N/A'
+        $cpuCurrent = 'N/A'
 
         # Get the VM's threshold from the tag value
         try {
             $cpuThreshold = [double]$vm.cpuThreshold
         } catch {
-            $cpuThreshold = 'N/A'
             $isValid = $false
         }
 
         # Get the VM's metrics
-        $metrics = Get-AzMetric -ResourceId $vm.Id -MetricName 'Percentage CPU' -TimeGrain 00:01:00 -WarningAction SilentlyContinue
-        $cpuCurrent = ($metrics.Data | Measure-Object -Maximum -Property Average).Maximum
-
-        # Determine if the metrics where collected correctly
-        if (-not $cpuCurrent) {
-            $cpuCurrent = 'N/A'
+        try {
+            $metrics = Get-AzMetric -ResourceId $vm.Id -MetricName 'Percentage CPU' -TimeGrain 00:01:00 -WarningAction SilentlyContinue -ErrorAction Stop
+            $cpuCurrent = ($metrics.Data | Measure-Object -Maximum -Property Average -ErrorAction Stop).Maximum
+            if (-not $cpuCurrent) { throw 'Cannot query metrics' }
+        } catch {
             $isValid = $false
+            $cpuCurrent = 'N/A'
         }
 
         # Determine if the max CPU usage was below the threshold
         if ($isValid) {
-            $isBelowThreshold = $cpuThreshold -gt $cpuCurrent
+            $isBelowThreshold = $cpuCurrent -le $cpuThreshold
         } else {
             $isBelowThreshold = $false
         }
 
         # Create the report array
         $vm | Select-Object subscriptionId, resourceGroup, name, @{N = 'isValid'; E = { $isValid } },
-        @{N = 'isBelowThreshold '; E = { $isBelowThreshold } }, @{N = 'cpuThreshold'; E = { $cpuThreshold } }, @{N = 'cpuCurrent'; E = { $cpuCurrent } }
-    }
-
-    # Deallocate the relevant VMs
-    if ($DeallocateVMs) {
-        $report | Where-Object { $_.isBelowThreshold } | For-Object {
-            Stop-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force -NoWait | Out-Null
-        }
+        @{N = 'isBelowThreshold'; E = { $true -eq $isBelowThreshold } }, @{N = 'cpuThreshold'; E = { $cpuThreshold } }, @{N = 'cpuCurrent'; E = { $cpuCurrent } }
     }
 
     # Display report
     $report
+
+    # Deallocate the relevant VMs
+    if ($DeallocateVMs) {
+        $vmsToDeallocate = @($report | Where-Object { $_.isBelowThreshold })
+        Write-Output (("Deallocating {0} VM(s) with CPU usage below threshold") -f $vmsToDeallocate.Count)
+        $vmsToDeallocate | ForEach-Object {
+            Write-Output ("Stopping {0}\{1}" -f $_.ResourceGroup, $_.Name)
+            Stop-AzVM -ResourceGroupName $_.ResourceGroup -Name $_.Name -Force -NoWait
+        }
+    } else {
+        Write-Output ("{0} VM(s) have a CPU usage below the threshold and would have been shutdown" -f $vmsToDeallocate.Count)
+    }
 
 } catch {
     Write-Output ($_)
