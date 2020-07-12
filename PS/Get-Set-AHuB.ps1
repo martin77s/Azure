7a5771629b02fcb1edb8d1f36f8f6fc4ff47012c
Martin Schvartzman <martin77s@gmail.com>
2020-07-12 11:34:28 +0300

    content updates

--- PS/Get-Set-AHuB.ps1
+++ PS/Get-Set-AHuB.ps1
@@ -1,18 +1,30 @@
 # Get all Windows Servers not using AHBU:
 function Get-AzVmNotAHuB {
-    param($ResourceGroupName = '*')
-    Get-AzVM -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue |
-    Where-Object { $_.StorageProfile.OsDisk.OsType -eq 'Windows' -and @('Windows_Server', 'Windows_Client') -notcontains $_.LicenseType }
+    param($SubscriptionNamePattern = '.*', $ResourceGroupName = '*')
+
+    Get-AzSubscription | Where-Object { ($_.Name -match $SubscriptionNamePattern) -and ($_.State -eq 'Enabled') } | ForEach-Object {
+        Write-Verbose ('Switching to subscription: {0}' -f $_.Name) -Verbose
+        $null = Set-AzContext -SubscriptionObject $_ -Force
+
+        Get-AzVM -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue |
+            Where-Object { $_.StorageProfile.OsDisk.OsType -eq 'Windows' -and @('Windows_Server', 'Windows_Client') -notcontains $_.LicenseType }
+    }
 }
 
 
 # Set all Windows Servers to use AHUB:
 function Set-AzVmAHuB {
-    param($ResourceGroupName = '*')
-    Get-AzVM -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue |
-    Where-Object { $_.StorageProfile.OsDisk.OsType -eq 'Windows' -and @('Windows_Server', 'Windows_Client') -notcontains $_.LicenseType } |
-    ForEach-Object {
-        $_.LicenseType = 'Windows_Server'
-        $_ | Update-AzVM
+    param($SubscriptionNamePattern = '.*', $ResourceGroupName = '*')
+
+    Get-AzSubscription | Where-Object { ($_.Name -match $SubscriptionNamePattern) -and ($_.State -eq 'Enabled') } | ForEach-Object {
+        Write-Verbose ('Switching to subscription: {0}' -f $_.Name) -Verbose
+        $null = Set-AzContext -SubscriptionObject $_ -Force
+
+        Get-AzVM -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue |
+        Where-Object { $_.StorageProfile.OsDisk.OsType -eq 'Windows' -and @('Windows_Server', 'Windows_Client') -notcontains $_.LicenseType } |
+        ForEach-Object {
+            $_.LicenseType = 'Windows_Server'
+            $_ | Update-AzVM
+        }
     }
 }
