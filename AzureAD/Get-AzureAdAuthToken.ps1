52a5b520dc960f1125ca4a2c23129eb026593c90
Martin Schvartzman <martin.schvartzman@microsoft.com>
2021-02-08 23:31:08 +0200

    content updates

--- AzureAD/Get-AzureAdAuthToken.ps1
+++ AzureAD/Get-AzureAdAuthToken.ps1
@@ -7,28 +7,28 @@ Last Update	: 2021/02/08
 Keywords	: AzureAD, AuthToken, GraphAPI
 References  : https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/how-to-use-vm-token
 
- ============[DISCLAIMER]=========================================================================================================
-  THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
-  INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
-  We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
-  code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software 
-  product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the 
-  Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or 
-  lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
- =================================================================================================================================
+============[DISCLAIMER]=========================================================================================================
+THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
+code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
+Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or
+lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
+=================================================================================================================================
 
 #>
 
 [CmdletBinding()]
 
 PARAM(
-    $TenantId = 'common',
+	$TenantId = 'common',
 	$TimeOut = 60
 )
 
 $localEpoch = (Get-Date -Date '1970/01/01 00:00:00').AddMinutes((Get-TimeZone).BaseUtcOffset.TotalMinutes)
 
-if(-not ($global:aadApi_token -and $localEpoch.AddSeconds(($global:aadApi_token).expires_on) -gt (Get-Date))) {
+if (-not ($global:aadApi_token -and $localEpoch.AddSeconds(($global:aadApi_token).expires_on) -gt (Get-Date))) {
 
 	$ClientID = '1950a258-227b-4e31-a9cf-717495945fc2' # Azure PowerShell
 	$Resource = 'https://graph.microsoft.com/'
@@ -65,7 +65,7 @@ if(-not ($global:aadApi_token -and $localEpoch.AddSeconds(($global:aadApi_token)
 		}
 
 	} while ((-not $token) -and ($TimeOut -gt 0))
-	
+
 	$global:aadApi_token = $token
 }
 
